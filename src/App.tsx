e5f7559b9076db01a065907c5a025ba91842e71c
Francis Lagares <francis.lagares@gmail.com>
2021-10-17 17:15:20 +0200

    feat: create Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/index.css';
 
 import Header from './components/header/Header';
+import Footer from 'components/footer/Footer';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <div style={{ backgroundColor: 'blue', height: '800px' }}></div>
       <div style={{ backgroundColor: 'green', height: '800px' }}></div>
       <div style={{ backgroundColor: 'yellow', height: '800px' }}></div>
+      <Footer />
     </div>
   );
 };
