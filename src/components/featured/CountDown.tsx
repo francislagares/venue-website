a7d0c79c8378febb7c017352f3559d130158b4bb
Francis Lagares <francis.lagares@gmail.com>
2021-10-20 21:00:16 +0200

    feat: implement countdown funcionality

--- src/components/featured/CountDown.tsx
+++ src/components/featured/CountDown.tsx
@@ -1,31 +1,55 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Slide } from 'react-awesome-reveal';
 
+interface ITime {
+  days: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
+}
+
 const CountDown = () => {
+  const [time, setTime] = useState<ITime>({
+    days: '0',
+    hours: '0',
+    minutes: '0',
+    seconds: '0',
+  });
+
   const settings = {
     left: true,
     delay: 1000,
   };
 
-  const renderItem = (time: number, value: string) => (
+  const renderItem = (time: string, value: string) => (
     <div className='countdown_item'>
       <div className='countdown_time'>{time}</div>
       <div className='countdown_tag'>{value}</div>
     </div>
   );
 
-  const getTimeUntil = (deadline: string) => {
+  const getTimeUntil = useCallback((deadline: string) => {
     const time = Date.parse(deadline) - Date.parse(new Date().toLocaleString());
 
     if (time < 0) {
       console.log('Date passed');
     } else {
-      console.log('Still going');
+      const seconds = Math.floor((time / 1000) % 60).toString();
+      const minutes = Math.floor((time / 1000 / 60) % 60).toString();
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24).toString();
+      const days = Math.floor(time / (1000 * 60 * 60 * 24)).toString();
+
+      setTime({
+        days,
+        hours,
+        minutes,
+        seconds,
+      });
     }
-  };
+  }, []);
 
   useEffect(() => {
-    setInterval(() => getTimeUntil('Nov, 20, 2021, 01:20:00'), 1000);
+    setInterval(() => getTimeUntil('Dec, 20, 2021, 01:20:00'), 1000);
   }, [getTimeUntil]);
 
   return (
@@ -33,10 +57,10 @@ const CountDown = () => {
       <div className='countdown_wrapper'>
         <div className='countdown_top'>Event starts in</div>
         <div className='countdown_bottom'>
-          {renderItem(27, 'Days')}
-          {renderItem(4, 'HS')}
-          {renderItem(10, 'Min')}
-          {renderItem(50, 'Sec')}
+          {renderItem(time.days, 'Days')}
+          {renderItem(time.hours, 'HS')}
+          {renderItem(time.minutes, 'Min')}
+          {renderItem(time.seconds, 'Sec')}
         </div>
       </div>
     </Slide>
