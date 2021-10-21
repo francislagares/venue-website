5452b56c1d179e3c4556efdbbec5b6a133d0e1b9
Francis Lagares <francis.lagares@gmail.com>
2021-10-21 21:24:03 +0200

    feat: create VenueInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/index.css';
 import Featured from './components/featured/';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import VenueInfo from './components/venueInfo';
 
 const App = () => {
   return (
@@ -13,10 +14,16 @@ const App = () => {
       <>
         <Featured />
       </>
+
+      <>
+        <VenueInfo />
+      </>
+
       <div style={{ backgroundColor: 'red', height: '800px' }}></div>
       <div style={{ backgroundColor: 'blue', height: '800px' }}></div>
       <div style={{ backgroundColor: 'green', height: '800px' }}></div>
       <div style={{ backgroundColor: 'yellow', height: '800px' }}></div>
+
       <Footer />
     </div>
   );
