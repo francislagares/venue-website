529307514ca85247bbebf089d85c7778939bc14a
Francis Lagares <francis.lagares@gmail.com>
2021-10-23 20:33:19 +0200

    fix(tests): add IntersectionObserver mock class
    If you try to run tests with jest in a project that
    uses react-slick, you may run into this error:
    
    'matchMedia not present, legacy browsers require a polyfill'
    
    This commit solves the issue.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,38 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-empty-function */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
+// @ts-ignore
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
